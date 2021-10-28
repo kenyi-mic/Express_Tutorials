77e05cc228c8c03715c782239cee98b882e4175d
Michael Kenyi <michaelkenyi13@gmail.com>
2021-10-28 23:58:58 +0600

    form handling using browser functionalities

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,25 @@
 const handlers = require("./lib/handlers");
-
 const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
+const weatherMiddlware = require("./lib/middleware/weather");
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
 
 
 const port = process.env.PORT || 3000;
+
 
 app.engine(
   "handlebars",
   handlebars({
     extname: ".handlebars",
-    defaultLayout: false,
-    layoutsDir: "views/layouts/",
+    defaultLayout: 'main',
     helpers:{
-      secttion: function(name, options){
+      section: function(name, options){
         if(!this._sections)this._sections ={}
         this._sections[name] = options.fn(this)
         return null
@@ -25,10 +30,16 @@ app.engine(
 
 
 
-app.use(express.static(`${__dirname}/public/`));
 
 app.set("view engine", "handlebars");
 
+
+
+app.use(weatherMiddlware)
+
+
+
+app.use(express.static(`${__dirname}/public/`));
 
 app.get("/", handlers.home);
 
@@ -36,17 +47,22 @@ app.get("/about", handlers.about);
 
 app.get("/contact", handlers.contact);
 
+app.get('/faq', handlers.faq);
 
-// exports.getFortune = (idx) => {
-//   idx = Math.floor(Math.random()* fortune.length)
+app.get('/newsletter', handlers.newsletter);
 
-//   }
+
+
 // custom 404 page
 app.use(handlers.notFound);
 
 // custom 500 page
 app.use(handlers.serverError);
 
+// browser based form handler
+app.get("/newsletter-signup", handlers.newsletterSignup)
+app.get("/newsletter-signup/process", handlers.newsletterSignupProcess)
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou)
 
 
 if (require.main === module) {
