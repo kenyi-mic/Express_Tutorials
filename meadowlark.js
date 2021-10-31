233c1afb159beb9467741eacaa7238f13a3aaede
Michael Kenyi <michaelkenyi13@gmail.com>
2021-10-31 18:27:16 +0600

    added weather API and fetched it

--- meadowlark.js
+++ meadowlark.js
@@ -4,40 +4,29 @@ const handlebars = require("express-handlebars");
 const app = express();
 const weatherMiddlware = require("./lib/middleware/weather");
 
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
-
 
 app.engine(
   "handlebars",
   handlebars({
     extname: ".handlebars",
-    defaultLayout: 'main',
-    helpers:{
-      section: function(name, options){
-        if(!this._sections)this._sections ={}
-        this._sections[name] = options.fn(this)
-        return null
+    defaultLayout: "main",
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
       },
     },
   })
 );
 
-
-
-
 app.set("view engine", "handlebars");
 
-
-
-app.use(weatherMiddlware)
-
-
+app.use(weatherMiddlware);
 
 app.use(express.static(`${__dirname}/public/`));
 
@@ -47,11 +36,9 @@ app.get("/about", handlers.about);
 
 app.get("/contact", handlers.contact);
 
-app.get('/faq', handlers.faq);
+app.get("/faq", handlers.faq);
 
-app.get('/newsletter', handlers.newsletter);
-
-
+app.get("/newsletter", handlers.newsletter);
 
 // custom 404 page
 app.use(handlers.notFound);
@@ -60,10 +47,9 @@ app.use(handlers.notFound);
 app.use(handlers.serverError);
 
 // browser based form handler
-app.get("/newsletter-signup", handlers.newsletterSignup)
-app.get("/newsletter-signup/process", handlers.newsletterSignupProcess)
-app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou)
-
+app.get("/newsletter-signup", handlers.newsletterSignup);
+app.get("/newsletter-signup/process", handlers.newsletterSignupProcess);
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
 
 if (require.main === module) {
   app.listen(port, () =>
